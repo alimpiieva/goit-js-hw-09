d0d400147e550471b293790cb16704fa8ee9ea52
eXis13 <aliksukerch@gmail.com>
2023-05-24 22:55:19 +0300

    hi

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,15 +7,15 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
+  onClose(selectedDates) {
+    let selectedDate = selectedDates[0];
 
-        if (selectedDate < new Date()) {
-            Notiflix.Notify.warning("Please choose a date in the future");
-        } else {
-                startButton.disabled = false;
-        }
-    },
+    if (selectedDate < new Date()) {
+      Notiflix.Notify.warning("Please choose a date in the future");
+    } else {
+      startButton.disabled = false;
+    }
+  },
 };
 
 const datePicker = flatpickr("#datetime-picker", options);
@@ -23,18 +23,17 @@ const datePicker = flatpickr("#datetime-picker", options);
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
-
-
+let selectedDate;
 
 startButton.addEventListener('click', () => {
-  const targetDate = datePicker.selectedDates[0];
+  selectedDate = datePicker.selectedDates[0];
   const countdownInterval = setInterval(updateCountdown, 1000);
 
   updateCountdown();
 
-    function updateCountdown() {
+  function updateCountdown() {
     const currentDate = new Date();
-    const timeRemaining = targetDate - currentDate;
+    const timeRemaining = selectedDate - currentDate;
 
     if (timeRemaining <= 0) {
       clearInterval(countdownInterval);
@@ -84,7 +83,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
